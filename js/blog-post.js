0e99d7839b19e65b47803f3c911271e00fd57f7c
VReinhaug <v.reinhaug@gmail.com>
2024-05-01 21:35:24 +0200

    image funtion

--- js/blog-post.js
+++ js/blog-post.js
@@ -6,15 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const baseUrl = `https://www.veronika-codes.one/wp-json/wp/v2/posts?slug=${slug}&_embed`;
 
     const postContainer = document.querySelector(".specific-post");
-    const featuredImage = document.getElementById("featuredImage");
-    const postTitle = document.getElementById("postTitle");
-    const postContent = document.getElementById("postContent");
 
     async function getPost() {
         try {
             const response = await fetch(baseUrl);
             const postDetails = await response.json();
-            console.log(postDetails);
 
             if (!postDetails || postDetails.length === 0) {
                 throw new Error("Post not found");
@@ -24,10 +20,39 @@ document.addEventListener("DOMContentLoaded", function() {
                                             <img src="${postDetails[0]._embedded["wp:featuredmedia"][0].source_url}" alt="${postDetails[0]._embedded["wp:featuredmedia"][0].alt_text}">
                                         </div>
                                         <H1>${postDetails[0].title.rendered}</H1>
-                                        <div>${postDetails[0].content.rendered}</div>`;
+                                        <div class="content">${postDetails[0].content.rendered}</div>`;
 
-            // Change page title
+            // Change title
             document.title = "My travel blog | " + postDetails[0]._embedded["wp:term"][1][0].name;
+
+            // Add event listener to images
+            document.querySelectorAll(".specific-post .content .wp-block-image img").forEach(img => {
+                img.addEventListener("click", function() {
+                    const modalHtml = `
+                        <div id="myModal" class="modal">
+                            <span class="close">&times;</span>
+                            <img class="modal-content" id="expandedImg">
+                        </div>
+                    `;
+                    document.body.insertAdjacentHTML('beforeend', modalHtml);
+                    const modal = document.getElementById("myModal");
+                    const modalContent = document.getElementById("expandedImg");
+                    modalContent.src = this.src;
+
+                    // Close when clicking x
+                    const closeBtn = document.querySelector(".close");
+                    closeBtn.addEventListener("click", function() {
+                        modal.remove();
+                    });
+
+                    // Close when clicking outside image
+                    window.addEventListener("click", function(event) {
+                        if (event.target == modal) {
+                            modal.remove();
+                        }
+                    });
+                });
+            });
 
         } catch (error) {
             postContainer.innerHTML = message("error");
